21020df66b6417e7dbdbc08152c355871fce3ca9
Саша <a.vasilievaaa@yandex.ru>
2022-08-15 21:32:50 +0300

    первый коммит

---
+++ src/main/java/com/example/course345/Course345Application.java
@@ -0,0 +1,13 @@
+package com.example.course345;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course345Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course345Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/course345/Course345ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course345;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course345ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
